926553fcb4c4dc65913ae423c2fe6057de4ff60d
avishka <avishka@kali>
2023-01-28 22:16:51 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'bg-color': '#1E2235',
+      'card-color': '#23263A',
+      'pink': '#C14D69',
+      'gray': '#BABCC1',
+      'white': '#F5F5F7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
